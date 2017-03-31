e99af2fcb501817d0638f395c1aca34130fe8628
nekowen <nekonyanowen@gmail.com>
2017-03-31 19:35:29 +0900

    commit Pods directory

---
+++ Example/Pods/Target Support Files/GeoHex3.swift/GeoHex3.swift-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_GeoHex3_swift : NSObject
+@end
+@implementation PodsDummy_GeoHex3_swift
+@end
---
+++ Example/Pods/Target Support Files/Pods-GeoHex3.swift_Tests/Pods-GeoHex3.swift_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GeoHex3_swift_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_GeoHex3_swift_Tests
+@end
